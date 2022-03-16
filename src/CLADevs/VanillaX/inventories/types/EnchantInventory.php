d25cedc04e61ababf35ba70a294fbe63bd596c49
SD <95081674+sdsd16@users.noreply.github.com>
2022-03-17 07:52:23 +0800

    Fix Enchant bug
    After Enchanted and rejoin server , the xp level set the same level before enchant.

--- src/CLADevs/VanillaX/inventories/types/EnchantInventory.php
+++ src/CLADevs/VanillaX/inventories/types/EnchantInventory.php
@@ -15,10 +15,21 @@ class EnchantInventory extends FakeBlockInventory{
         parent::__construct($holder, 2, BlockLegacyIds::AIR, WindowTypes::ENCHANTMENT);
     }
 
+    public function handlePacket(Player $player, ServerboundPacket $packet): bool{
+        if($packet instanceof ActorEventPacket && $packet->eventId === ActorEvent::PLAYER_ADD_XP_LEVELS){
+            if(!$player->isCreative()){
+                $player->getXpManager()->setXpLevel($player->getXpManager()->getXpLevel() - abs($packet->eventData));
+            }
+        }
+        return true;
+    }
+
+
     /**
      * @param Player $player, returns player who successfully enchanted their item
      * @param Item $item, returns a new item after its enchanted
      */
     public function onSuccess(Player $player, Item $item): void{
+    //Server::getInstance()->getLogger()->info("[".date("Y/m/d:H:i:s")."] > {$player->getName()}進行了附魔");
     }
 }
